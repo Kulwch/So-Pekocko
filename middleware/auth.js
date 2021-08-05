fc026bbddef55786a81c1c6ea35d00c24cef35be
Kulwch <76691359+Kulwch@users.noreply.github.com>
2021-08-05 15:39:59 +0200

    Forgot to withdrad Token key from auth middleware. Now done, and put in the dotenv file.

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SO_SECRET_THAT_ITS_SECRET_IS_ALSO_A_SECRET');
+    const decodedToken = jwt.verify(token, process.env.TOKEN_KEY.toString());
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw Error;
